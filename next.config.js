eea668d4ad4cd4796ae5c8c2b1788ae0355bd58c
seolranlee <seolran.lee@kreamcorp.com>
2025-11-07 23:05:53 +0900

    feat: tailwind-nextjs-starter-blog 초기 셋팅 적용

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { withContentlayer } = require('next-contentlayer2')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
