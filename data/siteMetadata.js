8821ba3241ba1e1d0b3d4ac0bc8bc1deb77889cb
seolranlee <seolran.lee@kreamcorp.com>
2025-11-08 16:43:32 +0900

    feat: 기존 블로그 글 마이그레이션

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,7 +22,8 @@ const siteMetadata = {
   instagram: 'https://www.instagram.com',
   medium: 'https://medium.com',
   bluesky: 'https://bsky.app/',
-  locale: 'en-US',
+  // locale: 'en-US',
+  locale: 'ko-KR',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
   analytics: {
